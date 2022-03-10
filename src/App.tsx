09e0324ed3f3cea8d4c381504fa140a0d7018411
Lukasz Szmytko <lszmytko@gmail.com>
2022-03-10 19:23:47 +0100

    thousands added

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ const data = {
     { digits: 90, digitsConverted: "ninety" },
     { digits: 100, digitsConverted: "hundred" },
   ],
-  
 };
 
 function App() {
@@ -42,54 +41,85 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [convertedNumber, setConvertedNumber] = useState("");
 
-  const findConvertedNumberInData = (digits: number)=>{
-    return (data.digits.find((item) => item.digits === digits) as {
-      digits: number;
-      digitsConverted: string;
-    }).digitsConverted
-  }
+  const findConvertedNumberInData = (digits: number) => {
+    return (
+      data.digits.find((item) => item.digits === digits) as {
+        digits: number;
+        digitsConverted: string;
+      }
+    ).digitsConverted;
+  };
 
-  const convertOneDigit = (digits: number, zeroIncluded: boolean = true)=>{
-    if(zeroIncluded) return findConvertedNumberInData(digits)
-    else return ""
-  }
+  const convertOneDigit = (digit: number, zeroIncluded: boolean = true) => {
+    if (zeroIncluded || digit) return findConvertedNumberInData(digit);
+    return "";
+  };
 
-  const convertTwoDigits = (digits: number)=>{
-    if(digits >= 11 && digits < 20) return findConvertedNumberInData(digits);
-    
-    const digitsAsString = digits.toString();
-    const firstDigit = digitsAsString[0];
-    const secondDigit = digitsAsString[1]; 
-    let result : string
-    result = findConvertedNumberInData(+firstDigit * 10)  + (+secondDigit === 0 ? "" : "-" + convertOneDigit(+secondDigit));
-    return result
-  }
+  const convertTwoDigits = (digits: number) => {
+    if (digits >= 11 && digits < 20) return findConvertedNumberInData(digits);
+    if (digits.toString().length === 1) return convertOneDigit(digits, false);
+    console.log(digits);
 
-  const convertThreeDigits = (digits: number)=>{
     const digitsAsString = digits.toString();
     const firstDigit = digitsAsString[0];
     const secondDigit = digitsAsString[1];
-    const thirdDigit = digitsAsString[2];
-    let result : string
-    result = findConvertedNumberInData(+firstDigit * 10)
+    if (+firstDigit === 0) return convertOneDigit(+secondDigit, false);
+    console.log("ok");
+    const result =
+      findConvertedNumberInData(+firstDigit * 10) +
+      (!+secondDigit ? "" : "-") +
+      (+secondDigit === 0 ? "" : convertOneDigit(+secondDigit, false));
+    return result;
+  };
+
+  const convertHundreds = (digits: number) => {
+    const digitsAsString = digits.toString();
+    if (digitsAsString.length === 1) return convertOneDigit(digits);
+    if (digitsAsString.length === 2) return convertTwoDigits(digits);
+    if (digitsAsString.length === 3) {
+      const firstDigit = digitsAsString[0];
+      const otherDigits = digitsAsString.slice(1);
+      let result: string;
+      const otherDigitsConversionResult = convertTwoDigits(+otherDigits);
+      result =
+        convertOneDigit(+firstDigit, false) +
+        " hundred " +
+        (otherDigitsConversionResult.length > 0 ? " and " : "") +
+        otherDigitsConversionResult;
+      return result;
+    }
+    return "s";
+  };
+
+  const convertThousands = (digits: number) => {
+    console.log("przeszlo");
+    const digitsAsString = digits.toString();
+    const hundreds = digitsAsString.toString().slice(-3);
+    const thousands = digitsAsString.slice(0, digits.toString().length - 3);
+    const isHundredsOnlyZero = new Set(hundreds).size === 1 && new Set(hundreds).has("0") // check if only zero after 
+    console.log("thousands", thousands);
+    const hundredsToString = isHundredsOnlyZero ? "" : convertHundreds(+hundreds);
+    const thousandsToString = convertHundreds(+thousands);
+    return thousandsToString + " thousand " + hundredsToString;
+  };
+
+  const convertMillions = (digits: number)=>{
+
   }
 
   const showNumber = (numberasString: string | number) => {
     numberasString = (numberasString as string).trim();
     const numberasNumber = +numberasString;
-    if (isNaN(numberasNumber)) return setError("Podaj liczbę");
-    if(numberasNumber < 0 || Math.round(numberasNumber) !== numberasNumber){
-      return setError("Not a proper number!")
+    if (isNaN(numberasNumber)) return setError("Write a number");
+    if (numberasNumber < 0 || Math.round(numberasNumber) !== numberasNumber) {
+      return setError("Not a proper number!");
     }
 
-    if (numberasString.toString().length === 1)
-      return setConvertedNumber(
-       convertOneDigit(+numberasString)
-      );
+    if (numberasString.toString().length <= 3)
+      return setConvertedNumber(convertHundreds(+numberasString));
 
-    if (numberasString.toString().length === 2){
-      return setConvertedNumber(convertTwoDigits(numberasNumber))
-    };
+    if (numberasString.toString().length <= 6)
+      return setConvertedNumber(convertThousands(+numberasString));
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
